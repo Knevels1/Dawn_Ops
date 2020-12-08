de0371a7f4638fab46497f13aab50eefbfc1f3bb
Knevels1 <kyle.a.nevels@icloud.com>
2020-12-08 10:17:19 -0600

    created nav bar folder and files

--- src/components/DawnOps.js
+++ src/components/DawnOps.js
@@ -1,19 +1,17 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
-import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import "./DawnOps.css"
 
-export const Kennel = () => (
+export const DawnOps = () => (
     <>
         <Route render={() => {
             if (localStorage.getItem("dawnops_user")) {
                 return (
                     <>
                         <Route render={props => <NavBar {...props} />} />
-                        <Route render={props => <ApplicationViews {...props} />} />
                     </>
                 )
             } else {
